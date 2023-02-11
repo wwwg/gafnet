d761a426134960a933f817016e24ac2b90c9a370
wwwg <joshpauten@gmail.com>
2023-02-11 06:15:26 -0500

    develop gafnode struct a bit

--- gafnet/gafnode.h
+++ gafnet/gafnode.h
@@ -2,14 +2,22 @@
 #ifndef _GAF_NODE_H
 #define _GAF_NODE_H
 
+#define GAFNET_MAX_PEERS 50
+
 #include <netinet/in.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
 #include <sys/socket.h>
 
+#include <string.h>
+
 struct gafnode {
 	int port;
+	size_t peers_count;
+
+	int _peers_sock[GAFNET_MAX_PEERS];
+	int _listen_sock;
 };
 
 struct gafnode* init_gafnode(int);
